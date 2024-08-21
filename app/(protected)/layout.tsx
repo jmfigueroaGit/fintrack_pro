a1a6bc1ca90f471c850a03e76be06d5ad6bfc2f3
Jm Figueroa <figueroa.jheremiah@gmail.com>
2024-08-21 08:48:51 +0800

    Fixed layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -6,14 +6,12 @@ import React from 'react';
 const ProtectedLayout = async ({ children }: { children: React.ReactNode }) => {
 	return (
 		<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-			<div className='hidden md:block '>
-				<div className='border-t'>
-					<Header />
-					<div className='bg-background '>
-						<div className='col-span-3 lg:col-span-4 lg:border-l px-4 py-6 lg:px-8'>{children}</div>
-					</div>
-					<Toaster />
+			<div className='border-t'>
+				<Header />
+				<div className='bg-background '>
+					<div className='col-span-3 lg:col-span-4 lg:border-l px-4 py-6 lg:px-8'>{children}</div>
 				</div>
+				<Toaster />
 			</div>
 		</ThemeProvider>
 	);
